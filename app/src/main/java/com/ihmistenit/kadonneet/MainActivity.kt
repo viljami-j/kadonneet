4418379c2398010be6a404c0897915a1f9ef8d16
Viljami Mäkelä <viljami.j.m@gmail.com>
2023-01-23 20:43:47 +0200

    ...

--- app/src/main/java/com/ihmistenit/kadonneet/MainActivity.kt
+++ app/src/main/java/com/ihmistenit/kadonneet/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ihmistenit.kadonneet
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -21,12 +24,13 @@ import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.snackbar.Snackbar
 import com.ihmistenit.kadonneet.databinding.ActivityMainBinding
 import com.ihmistenit.kadonneet.ui.user_advert.UserAdvertListFragment
+import com.ihmistenit.kadonneet.ui.user_advert.UserAdvertsFragment
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
-    fun evaluateThemePreference(newVal: Any): Boolean {
+    private fun evaluateThemePreference(newVal: Any): Boolean {
         if (newVal == "auto") AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         if (newVal == "day") AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         if (newVal == "night") AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -43,22 +47,19 @@ class MainActivity : AppCompatActivity() {
         val themePreference = PreferenceManager.getDefaultSharedPreferences(applicationContext).getString("theme", "auto")
         if (themePreference != null) evaluateThemePreference(themePreference)
 
-        val test = supportFragmentManager.findFragmentByTag(UserAdvertListFragment::class.java.name)
-        if (test == null) println("elontusk")
-
-        val userAdvertRecyclerView: RecyclerView? = supportFragmentManager.findFragmentById(R.id.tabContentFragContainer)?.view?.findViewById(R.id.user_advert_recyclerview)
+        val userAdvertRecyclerView: Fragment? = supportFragmentManager.findFragmentById(R.id.tabContentFragContainer)//?.view?.findViewById(R.id.user_advert_recyclerview)
         if (userAdvertRecyclerView != null) {
-            userAdvertRecyclerView.setOnClickListener(View.OnClickListener {
-                println("Hello!")
-            })
+            //userAdvertRecyclerView.setOnClickListener(View.OnClickListener {
+            //    println("Hello!")
+            //})
+            println("Hello!")
         } else println("MainActivity.kt: Couldn't find userAdvertRecyclerView (Is it not occupying the tabContentFragmentPlaceholder at the moment?)")
 
-        // Setup bottom navigation
-        val navView: BottomNavigationView = binding.bottomNavView
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_bottom_nav) as NavHostFragment
-        navView.setupWithNavController(navHostFragment.navController)
-        navView.menu.findItem(R.id.nav_report_finding).isEnabled = false
+        setupBottomNavigation()
+        setupReportFindingFab()
+    }
 
+    private fun setupReportFindingFab() {
         // Setup report finding fab
         val fab: FloatingActionButton = binding.fabReportFinding
         fab.bringToFront()
@@ -66,6 +67,15 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(binding.root, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun setupBottomNavigation() {
+        // Setup bottom navigation
+        val navView: BottomNavigationView = binding.bottomNavView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_bottom_nav) as NavHostFragment
+        navView.setupWithNavController(navHostFragment.navController)
+        navView.menu.findItem(R.id.nav_report_finding).isEnabled = false
     }
 
     override fun onSupportNavigateUp(): Boolean {
